abdfda69a0fc8cf5bac6b4b96bf36e2f8880e3b6
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-04-20 14:02:56 -0400

    delete: removed test admin route

--- routes/admin.routes.ts
+++ routes/admin.routes.ts
@@ -6,9 +6,6 @@ const router: Router = Router();
 
 // All admin routes are prefixed with /admin
 router.use("/admin", [
-  router.get("/", async (_, res: Response): Promise<void> => {
-    res.json({ message: "Hello Admins!" });
-  }),
   router.get("/list", accountController.listAll),
 ]);
 
