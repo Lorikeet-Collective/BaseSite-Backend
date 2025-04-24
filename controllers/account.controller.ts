b101f426c79e287cbf66bfc69e26ee3b2687f3a0
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2025-04-24 18:32:32 -0400

    fix: add documentation for new controller login function

--- controllers/account.controller.ts
+++ controllers/account.controller.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcrypt";
 
 type LoginReq = Requests.Login;
 
+// General controller used for all admin / account functions
 const accountController = {
   listAll: async (_: unknown, res: Response): Promise<void> => {
     await Account.find()
@@ -13,25 +14,30 @@ const accountController = {
       .catch((err) => res.status(500).json(err));
   },
   login: async (req: LoginReq, res: Response): Promise<void> => {
+    // Check if account exists
     await Account.findOne({
       username: req.body.username,
     })
       .then(async (account) => {
         if (account) {
+          // Compare passwords
           await bcrypt
             .compare(req.body.password, account.password)
             .then((result) => {
               if (result) {
+                // Sign token on correct password
                 const token = Jwt.sign(
                   { userId: account._id, clearance: account.clearance },
                   process.env.SECRET_KEY,
                   { expiresIn: 1000 * 60 * 60 }
                 );
+                // Send token as header:Bearer
                 res
                   .set("Authorization", `Bearer ${token}`)
                   .status(200)
                   .json({ msg: "Logging In..." });
               } else {
+                // Send validation error on wrong password
                 res
                   .status(400)
                   .json(
@@ -45,6 +51,7 @@ const accountController = {
               }
             });
         } else {
+          // Send validation error on wrong username
           res
             .status(400)
             .json(
@@ -58,6 +65,7 @@ const accountController = {
         }
       })
       .catch((err) => {
+        // General catch-all error in the event of a server error
         res.status(500).json({ msg: "Something went wrong.", err: err });
       });
   },
